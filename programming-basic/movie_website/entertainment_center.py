d0f6aa34451bc0bb54168a42b6cab1487def6705
yunguang <574505399@qq.com>
2018-03-02 14:11:08 +0800

    修改项目

--- programming-basic/movie_website/entertainment_center.py
+++ programming-basic/movie_website/entertainment_center.py
@@ -2,9 +2,14 @@
 import fresh_tomatoes
 import media
 
+# spider_man,redemption,blade是调用Movie生成的实例
 spider_man=media.Movie('Spider-Man','https://i.ytimg.com/vi/K4zm30yeHHE/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCZOvOupXYdQQVMQuIwHCnGSlk8yQ','https://www.youtube.com/watch?v=K4zm30yeHHE')
 redemption=media.Movie('The Shawshank Redemption','https://i.ytimg.com/vi/qQIQVPNcB_Q/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDH4nB7kVo0gqu2eSDira2ANfVqVg','https://www.youtube.com/watch?v=accOHNw_qX8')
 blade=media.Movie('Blade The Series','https://i.ytimg.com/vi/uMNL4h8hZEs/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCtw8MR-BV89BtMIwB3QJe77I71Iw','https://www.youtube.com/watch?v=uMNL4h8hZEs')
+
+# 将生成的实例放到movies列表中
 movies=[spider_man,redemption,blade]
+
+# 调用fresh_tomatoes模块的open_movies_page，传入movies列表，生成可以浏览的网页
 fresh_tomatoes.open_movies_page(movies)
 
--- programming-basic/movie_website/media.py
+++ programming-basic/movie_website/media.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*
 import webbrowser
 class Movie():
+    '''定义Python类Movie'''
     def __init__(self, movie_title, poster_image, trailer):
+        '''__init__是类Movie的构造函数，传入参数电影标题movie_title，封面图片地址poster_image，预览视频地址trailer，
+        最终生成实例的title，poster_image_url，trailer_youtube_url属性'''
         self.title=movie_title
         self.poster_image_url=poster_image
         self.trailer_youtube_url=trailer
     def show_trailer(self):
+        '''show_trailer函数调用Python内置模块webbrowser的open方法来打开浏览器'''
         webbrowser.open(self.trailer_youtube_url)
 
